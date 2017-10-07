3de6faff5cbca0c8d4fb01326bf16e93762a2968
amandeepmittal <amandeepmittal@live.com>
2017-10-07 19:27:41 +0530

    add mvp description

--- client/components/header.js
+++ client/components/header.js
@@ -30,18 +30,11 @@ const Header = () => {
 							id="navigation"
 							className="collapse navbar-collapse navbar-right"
 						>
-							{/* <ul className="nav navbar-nav">
+							<ul className="nav navbar-nav">
 								<li>
-									<a href="#link1" className="scroll-to">
-										Link1
-									</a>
+									<a href="">a URL Shortener Service</a>
 								</li>
-								<li>
-									<a href="#link2" className="scroll-to">
-										Link2
-									</a>
-								</li>
-							</ul> */}
+							</ul>
 						</div>
 					</div>
 				</div>
