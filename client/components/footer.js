7a6ebc9a32709653960fc194c2cfcc335e9e3505
amandeepmittal <amandeepmittal@live.com>
2017-10-07 17:50:08 +0530

    footer

---
+++ client/components/footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/footer.css';
+
+const Footer = () => {
+	return (
+		<div>
+			<footer id="footer">
+				<div className="container">
+					<div className="text-center">
+						<h2>Bitli</h2>
+						<p>Developed using Meteor + React</p>
+						<small>
+							Developed by Aman Mittal. Code is available on&nbsp;
+							<a href="https://github.com/amandeepmittal/meteor-bitli">
+								Github
+							</a>.
+						</small>
+					</div>
+				</div>
+			</footer>
+		</div>
+	);
+};
+
+export default Footer;
--- client/main.js
+++ client/main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header';
 import ShortLinksList from './components/shortlink-list';
+import Footer from './components/footer';
 
 const App = () => {
 	return (
 		<div className="App">
 			<Header />
 			<ShortLinksList />
+			<Footer />
 		</div>
 	);
 };
