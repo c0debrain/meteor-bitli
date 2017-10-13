9ef2c25e010350bd8dbf7b448769578d3f9c0096
amandeepmittal <amandeepmittal@live.com>
2017-10-13 12:20:59 +0530

    add notfound route and page

--- client/main.js
+++ client/main.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import App from './components/App';
+import NotFound from '../imports/ui/notFound';
 
 const routes = (
 	<Router history={browserHistory}>
 		<Route path="/" component={App} />
+		<Route path="*" component={NotFound} />
 	</Router>
 );
 
